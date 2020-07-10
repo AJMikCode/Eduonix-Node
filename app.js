9a006b31e60903445e90d9b5c477597e5b5b7605
Alexander Mikalauskas <40605024+AJMikCode@users.noreply.github.com>
2020-07-10 09:53:34 -0500

    NodeTutorials
    Includes .vs and node_modules for various tutorials given

--- app.js
+++ app.js
@@ -1,62 +1,43 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+//var pug = require("pug");
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
+var port = 3000;
 
-// Database Name
-const dbName = 'myproject';
-
-// Create a new MongoClient
-const client = new MongoClient(url);
-
-// Use connect method to connect to the Server
-client.connect(function (err, client) {
-    assert.equal(null, err);
-    console.log("Connected correctly to server");
-
-    const db = client.db(dbName);
-
-    const col = db.collection('bruh');
-
-// Insert a single document
-    //db.collection('goyotoyo').insertOne({ name:"Xander", age: 16, Hobby: "Video Games" }, function (err, r) {
-     //   assert.equal(null, err);
-     //   assert.equal(1, r.insertedCount);
-
-//Insert Multiples Documents
-   col.insertMany([{Character: "Hibana"}, {Character: "Echo"}, {Character: "Lesion"}, { Character: "Mira" }], function (err, r) {
-        assert.equal(null, err);
-        assert.equal(4, r.insertedCount);
-
-        //console.log("Inserted " + r.ops.length + " documents");
-       // console.log(r);
-        //Gets first two documents that match the query
-//Finds everythings in database, can be specified with curly braces
-        //col.find().toArray(function (err, docs) {
-          //  assert.equal(null, err);
-           // assert.equal(36, docs.length);
-
-           // console.log(docs);
-       col.updateOne({ Character: "Hibana" }, { $set: { Character: "Jackal" } }, function (err, r) {
-           assert.equal(null, err);
-           assert.equal(1, r.matchedCount);
-           assert.equal(1, r.modifiedCount);
-//Deletes One document inside Database
-          // col.deleteOne({ Character: "Echo" }, function (err, r) {
-           //    assert.equal(null, err);
-             //  assert.equal(1, r.deletedCount);
-           col.deleteMany({ Character: "Jackal", Character: "Mira"  }, function (err, r) {
-               assert.equal(null, err);
-               assert.equal(3, r.deletedCount);
+var app = express();
 
 
-               client.close();
-                });
-           });
-       });
-   });
-//});
-//Updates Documents
+app.use(function(_req, _res, next) {
+    console.log("Time: ", Date.now());
+    next();
+});
 
+//app.set("view engine", "pug");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, 'views'));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public page")));
+
+app.get('/', function(_req, res){
+    res.render("index", {
+        title: "Hello Wold",
+        showTitle: true,
+        people: ['John', 'Raquan', 'Jesus']
+    });
+});
+
+app.get('/about', function (_req, res) {
+    res.render('about');
+});
+
+app.get('/contact', function (_req, res) {
+    res.render('contact');
+});
+
+app.listen(port);
+console.log("Server started on port " + port);
+
+module.exports = app;
